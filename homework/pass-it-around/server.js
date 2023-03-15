3764a1f1440447dc229a77d295e5fba17b9d8353
Michelle Lee <lee.mxvii@gmail.com>
2023-03-15 01:15:05 -0500

    chef's kiss

--- homework/pass-it-around/server.js
+++ homework/pass-it-around/server.js
@@ -6,22 +6,42 @@ const PORT = 3002;
 
 let startingNum = 99; 
 
+
+
 app.get("/", (req, res) => {
     res.send(`<h1> ${startingNum} 
-    <span style= {{ textDecoration: 'line-through' }} problems but a bitch ain't one</span> 
-    bottles of better on the wall 
-    </h1>
+    <del> problems but a bitch ain't one</del> 
+    bottles of better on the wall </h1>
     <a href="${startingNum}"> take one down, pass it around
-    </a>`)
-
+    </a>`
+    )
 })
 
 app.get("/:number_of_bottles", (req, res) => {
     let currentNum = req.params.number_of_bottles
     if (currentNum > 1) {
-
+      res.send(`<h1> ${currentNum} 
+      <del> problems but a bitch ain't one</del>
+      bottles of beer on the wall</h1> 
+        <a href="${currentNum - 1}"> 
+          take one down, pass it around
+        </a>`
+      )
+    } else if (currentNum == 1){
+      res.send(`<h1> ${currentNum} 
+      <del> problem but a bitch STILL ain't one</del>
+      bottle of beer on the wall </h1>
+        <a href="${currentNum - 1}"> 
+        take one down, pass it around
+        </a> `
+      )
+    } else {
+      res.send(`<h1> There's ${currentNum} bottles!
+      <del> and no more problems!</del>
+       you passed around too many... </h1>
+        <a href="/"> grab some more bottles! </a> `
+      )
     }
-    res.send(`${number_of_bottles} problems but a bitch ain't one`)
 })
 
 app.listen(PORT, () => {
